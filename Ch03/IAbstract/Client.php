455803b624d76ac6b519f5ab8ca5c5f4cceb21cd
INOCÊNCIO QUISSUMUA CARDOSO <inocenciocardoso19@gmail.com>
2018-10-14 14:08:59 -0300

    Cliente

--- Ch03/IAbstract/Client.php
+++ Ch03/IAbstract/Client.php
@@ -1,6 +1,7 @@
 <?php
 include_once('NorthRegion.php');
 include_once('WestRegion.php');
+include_once('SouthRegion.php');
 
 class Client
 {
@@ -8,9 +9,11 @@ class Client
 	{
 		$north = new NorthRegion();
 		$west = new WestRegion();
+		$south = new SouthRegion();
 
 		$this->showInterface($north);
 		$this->showInterface($west);
+		$this->showInterface($south);
 	}
 
 	private function showInterface(IAbstract $region)
